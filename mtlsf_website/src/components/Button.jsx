655f3b9a0df4307d1b20ccd19cc8380a8a1dec7a
snorng1 <89033297+snorng1@users.noreply.github.com>
2023-03-15 18:46:53 -0400

    first commit

---
+++ mtlsf_website/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ( { styles } ) => {
+  return (
+    <button type='button' className={`py-4 px-6 bg-blue-gradient`}>
+      Get Started
+    </button>
+  )
+}
+
+export default Button
